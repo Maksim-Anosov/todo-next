190aab990d160114277b5b03682797e3aa787f4c
Maksim Anosov <eastcoastcustoms@yandex.ru>
2024-12-12 18:12:35 +0300

    initial

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,28 @@
+import { Vk, Telegram, Mail } from "../assets/index";
+
+export default function Header() {
+	return (
+		<header className="text-center text-2xl h-36 mb-auto flex justify-center items-center gap-2">
+			<a
+				className="hover:scale-125 transition-all"
+				href="https://t.me/AnsaksS"
+				target="_blank"
+			>
+				<Telegram />
+			</a>
+			<a
+				className="hover:scale-125 transition-all"
+				href="https://vk.com/id46614753"
+				target="_blank"
+			>
+				<Vk />
+			</a>
+			<a
+				href="mailto:eastcoastcustoms@yandex.ru"
+				className="hover:scale-125 transition-all"
+			>
+				<Mail />
+			</a>
+		</header>
+	);
+}
