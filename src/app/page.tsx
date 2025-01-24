ea6220827c67bea8dcd27969a728a524fce1b89d
Maksim Anosov <eastcoastcustoms@yandex.ru>
2025-01-24 14:12:09 +0300

    weather

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import { Todos } from "@/components";
 export default function App() {
 	return (
 		<section className="w-[75%] h-full mx-auto flex flex-col justify-between">
-			<div>
-				<h2 className="text-center text-5xl">TODOS</h2>
-			</div>
 				<Todos />
 		</section>
 	);
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,47 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+function getCurrentPositionAsync(): Promise<GeolocationPosition> {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+export async function getLocation() {
+  try {
+    const position = await getCurrentPositionAsync();
+    const { latitude, longitude } = position.coords;
+    const data = await getTempAndCityname(
+      String(latitude),
+      String(longitude)
+    );
+    return data;
+  } catch (error) {
+    console.error("Ошибка получения местоположения:", error);
+  }
+}
+
+export type Weather = {
+  temp: number
+  city: string
+};
+
+function getTempAndCityname(latitude: string, longitude: string): Promise<Weather> {
+  const url = `https://api.weatherapi.com/v1/current.json?key=566ce730aaf349ad82a95230252401&q=${latitude},${longitude}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      return {
+        temp: data.current.temp_c,
+        city: data.location.name,
+      };
+    })
+    .catch((error) => {
+      throw new Error(`Ошибка при получении температуры и названия города: ${error.message}`);
+    });
 }
