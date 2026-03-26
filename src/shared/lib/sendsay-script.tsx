61c53727c0a8ec5a102a725e279ef63f53a892c0
ansaks <whoami.ansaks@gmail.com>
2026-03-26 12:04:50 +0300

    updated script sendsay

--- src/shared/lib/sendsay-script.tsx
+++ src/shared/lib/sendsay-script.tsx
@@ -1,20 +1,11 @@
-import Script from 'next/script'
+import Script from "next/script";
 
 export const SendsayScript = () => {
   return (
     <Script id="sendsay-sdk" strategy="afterInteractive">
       {`
-        (function(a,b,c,d){
-          var e=a.getElementsByTagName(b)[0],
-              f=a.createElement(b);
-          f.async=!0;
-          f.src="https://image.sendsay.ru/app/js/sdk/sdk.min.js";
-          f.id="sendsay-sdk-script";
-          f.dataset.accountId=c;
-          f.dataset.siteId=d;
-          e.parentNode.insertBefore(f,e);
-        })(document,"script","x_1674462300972634","pl42397");
+        (function(a,b,c,d){var e=a.getElementsByTagName(b)[0],f=a.createElement(b);f.async=!0,f.src="https://image.sendsay.ru/app/js/sdk/sdk.min.js",f.id="sendsay-sdk-script",f.dataset.accountId=c,f.dataset.siteId=d,e.parentNode.insertBefore(f,e)})(document,"script","x_166049427987191","pl45478");
       `}
     </Script>
-  )
-}
+  );
+};
